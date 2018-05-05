6af5f81d181f4ef24ba20b22885a0560cf5e759e
guardezi <jefersonguardezi@gmail.com>
2018-05-04 22:14:11 -0300

    fix constants

--- src/providers/constants/constants.ts
+++ src/providers/constants/constants.ts
@@ -2,13 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConstantsProvider {
-  public API: string = 'https://api2.cerveja.me/api/';
-  // public API: string = 'http://localhost:9001/api/';
-  // public API: string = 'http://ec2-54-207-94-27.sa-east-1.compute.amazonaws.com/api/';
-
-  //  public API:string='http://localhost:1337/ec2-54-207-94-27.sa-east-1.compute.amazonaws.com/api/';
-  //  public API:string='http://10.42.0.1:9001/api/';
-  // public API: string = 'http://999eb3d2.ngrok.io/api/';
+  public API: string = 'https://api.hom.cerveja.me/api/';
   public DEVICE: string = 'device/';
   public LOCATION: string = 'location/';
   public PROFILE: string = 'profile/';
