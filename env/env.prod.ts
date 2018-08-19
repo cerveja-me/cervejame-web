d57e436caebfa5163c57089dce5f54b9a372c435
guardezi <jefersonguardezi@gmail.com>
2018-08-19 19:08:41 -0300

    fix env

--- env/env.prod.ts
+++ env/env.prod.ts
@@ -1,3 +1,3 @@
 export const env = { "server_url": "https://api.cerveja.me/api/", "fb_id": '339667689763204' };
 export const mobile = false;
-export const version = "3.1.14";
+export const appVersion = "3.1.14";
